b770b2c140b84b504f8b10c966520fac6aa2cd67
Fabio Rehm <fgrehm@gmail.com>
2013-01-18 00:49:38 -0200

    Add jquery notifier

--- test-app/application_controller.rb
+++ test-app/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   append_view_path File.dirname(__FILE__)
 
   def index
+    @suffix = params[:version].present? ? "-#{params[:version]}" : ''
     render :template => 'index'
   end
 end

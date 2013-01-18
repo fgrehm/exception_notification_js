4bda61bcfc0b4d95a5ec2244711f1bbee7cc110b
Fabio Rehm <fgrehm@gmail.com>
2013-01-17 23:27:03 -0200

    Add rails engine

--- lib/exception_notification_js.rb
+++ lib/exception_notification_js.rb
@@ -1,3 +1,4 @@
+require "exception_notification_js/engine"
 require "exception_notification_js/version"
 
 module ExceptionNotificationJS
---
+++ lib/exception_notification_js/engine.rb
@@ -0,0 +1,5 @@
+module ExceptionNotificationJS
+  class Engine < ::Rails::Engine
+    isolate_namespace ExceptionNotificationJS
+  end
+end
